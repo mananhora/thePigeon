68a083a6587e35463966815a5029b50d439caadd
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-18 14:59:50 -0500

    Script to create accounts.

---
+++ server/register.php
@@ -0,0 +1,29 @@
+<?php
+   // Code design from http://www.tutorialspoint.com/sqlite/sqlite_php.htm
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('thepigeon.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      //echo "Opened database successfully\n";
+   }
+   // Data obtained from AJAX:
+   // 1. $_POST['firstname'];
+   // 2. $_POST['lastname'];
+   // 3. $_POST['email'];
+   // 4. $_POST['password'];
+   $sql = "INSERT INTO user (username, password, firstname, lastname) VALUES (".$_POST['email'].$_POST['password'].$_POST['firstname'].$_POST['lastname'].")";
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Account created successfully.";
+   }
+   $db->close();
+?>
