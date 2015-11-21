5dc3e006440321a7c7eb32652808cd5bfaab75fd
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-21 16:25:51 -0500

    Add the friendrequest table

--- server/createdatabase.php
+++ server/createdatabase.php
@@ -18,6 +18,7 @@
    CREATE TABLE user (userid integer primary key autoincrement, username varchar(25) not null, password varchar(25) not null, firstname varchar(25) not null, lastname varchar(25) not null);
    CREATE TABLE letter (letterid integer primary key autoincrement, content varchar(1000) not null, timestamp default current_timestamp not null, userid integer not null, foreign key(userid) REFERENCES user(userid));
    CREATE TABLE mailbox (mailboxid integer primary key autoincrement, letterid integer not null, senderuserid integer not null, receiveruserid integer not null, timestamp default current_timestamp not null, foreign key(letterid) REFERENCES letter(letterid), foreign key(senderuserid) REFERENCES user(userid), foreign key(receiveruserid) REFERENCES user(userid));
+   CREATE TABLE friendrequest (friendrequestid integer primary key autoincrement, timestamp default current_timestamp not null, senderuserid integer not null, receiveruserid integer not null, foreign key(senderuserid) REFERENCES user(userid), foreign key(receiveruserid) REFERENCES user(userid));
    ";
    $ret = $db->exec($sql);
    if(!$ret){
