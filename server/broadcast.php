4801989f26ccdf17e8a8d9831fc7df58de274aa2
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-21 16:14:28 -0500

    Broadcasting works

--- server/broadcast.php
+++ server/broadcast.php
@@ -16,12 +16,43 @@
    // Data obtained from AJAX:
    // 1. $_POST["usernameVal"];
    // 2. $_POST["letter"];
-   $sql = "INSERT INTO letter (content, userid) VALUES ("."'".$_POST["letter"]."'".", '".$_POST["usernameVal"]."')";
+   // Obtain the userid of the user who wrote the letter.
+   $sqlgetsenderuserid = "SELECT userid FROM user WHERE username = '".$_POST["usernameVal"]."'";
+   $ret = $db->query($sqlgetsenderuserid);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+     $userid = $row['userid'];
+   }
+   // Store the letter.
+   $sql = "INSERT INTO letter (content, userid) VALUES ("."'".$_POST["letter"]."'".", '".$userid."')";
    $ret = $db->exec($sql);
    if(!$ret){
       echo $db->lastErrorMsg();
    } else {
-      echo "Letter stored successfully.";
+      //echo "Letter stored successfully.";
+   }  
+   // Obtain the letterid of the latest entry in the "letter" table, which corresponds to the letter that
+   // was just inserted/stored before using this script.
+   $sqlgetletterid = "SELECT letterid FROM letter";
+   $ret = $db->query($sqlgetletterid);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+     $letterid = $row['letterid'];
    }
-   $db->close();   
+   // Find the friends of the user currently logged in.
+   $sqlfriendrequestid = "SELECT receiveruserid FROM friendrequest WHERE senderuserid = ".$userid;
+   //$sqlfriendrequestid = "SELECT receiveruserid FROM friendrequest WHERE senderuserid = 1";
+   $ret = $db->query($sqlfriendrequestid);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      // Use each iteration to send the letter to each of the recipients of the broadcast.
+      //$receiveruserid .= $row['receiveruserid']." ";
+      $receiveruserid = $row['receiveruserid'];
+      $sqlsendletter = "INSERT INTO mailbox(letterid, senderuserid, receiveruserid) VALUES (".$letterid.", ".$userid.", ".$receiveruserid.");";
+      $result = $db->exec($sqlsendletter);
+      if(!$result){
+         echo $db->lastErrorMsg();
+      } else {
+         //echo "Letter stored successfully.";
+      } 
+   }
+   $db->close();
+   echo "Before".$receiveruserid."After".$sqlfriendrequestid."Now".$sqlsendletter;
 ?>
